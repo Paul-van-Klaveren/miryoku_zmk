95e52f3de48f19b1dddf8b48cca82bb9fbf42c1e
Paul-van-Klaveren <paul.van.klaveren@gmail.com>
2025-02-20 23:19:20 +0100

    Use existing macro factory `U_MACRO`

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,18 +1,7 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MACRO_ESCAPE_DOT \
-/ { \
-    macros { \
-        escape_dot: escape_dot { \
-            compatible = "zmk,behavior-macro"; \
-            #binding-cells = <0>; \
-            wait-ms = <30>; \
-        	tap-ms = <40>; \
-            bindings = <&macro_press &kp ESC>, <&macro_tap &kp DOT>, <&macro_release &kp ESC>; \
-        }; \
-    }; \
-};
+U_MACRO(u_macro_escdot, bindings = <&macro_press &kp ESC>, <&macro_tap &kp DOT>, <&macro_release &kp ESC>;)
 
 #define MIRYOKU_LAYER_BASE \
 U_MT(ESC, Q),      &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
@@ -23,5 +12,5 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 #define MIRYOKU_LAYER_MEDIA \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &u_out_tog,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_EP_TOG,          &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
-U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              U_NA,              U_NA,              U_NA,              MACRO_ESCAPE_DOT,      U_NA,              \
+U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              U_NA,              U_NA,              U_NA,              U_ESCDOT,          U_NA,              \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_N
